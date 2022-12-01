b3e652f17579818221e166f2737a2bdc05964670
amezir <amezirmessaoud.pro@gmail.com>
2022-12-01 22:32:58 +0100

    change name folder

--- scripts/main_app/info.js
+++ scripts/main_app/info.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-      fetch('data.json').then(function (response) {
+      fetch('moreinfo.js').then(function (response) {
           response.json().then(function (data) {
               data.forEach(function afficheAnalogie(resultat) {
                   document.querySelector('#cat-list').innerHTML += "";
