d8dac120c578f3e139590a25fc150bd3df910993
Chiuappebarr <104086635+Chiuappebarr@users.noreply.github.com>
2022-12-02 05:14:54 +0100

    Le site moche

---
+++ js/moche.js
@@ -0,0 +1,13 @@
+let bouton = document.getElementById("bouton");
+let traktopail = document.getElementById("traktopail");
+
+bouton.addEventListener("click", function() {
+bouton.style.display = "none";
+   
+});
+
+traktopail.addEventListener("click", function() {
+    
+traktopail.style.display = "none";
+   
+});
