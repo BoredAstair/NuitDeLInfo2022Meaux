f32b8747dbd4ace1f67833e52e72279e43a7664e
Chiuappebarr <104086635+Chiuappebarr@users.noreply.github.com>
2022-12-02 05:54:22 +0100

    modif js

--- js/moche.js
+++ js/moche.js
@@ -1,13 +1,7 @@
 let bouton = document.getElementById("bouton");
 let traktopail = document.getElementById("traktopail");
-
 bouton.addEventListener("click", function() {
-bouton.style.display = "none";
-   
+bouton.style.display = "none"; 
 });
-
 traktopail.addEventListener("click", function() {
-    
-traktopail.style.display = "none";
-   
-});
+traktopail.style.display = "none";});
